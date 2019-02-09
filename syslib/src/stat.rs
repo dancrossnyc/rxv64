3c51e512220def686b988ead7c20a7bd317e5be9
Dan Cross <cross@gajendra.net>
2019-02-09 03:45:15 +0000

    syslib: Common kernel and userspace definitions
    A trivial library providing a module for constants
    and so forth that are shared between the kernel and
    userspace.  For example, system call numbers are
    defined here.
    
    Both the kernel and ulib depend on syslib; having
    this keeps the dependency graph clean, as neither
    has to depend directly on the other.
    
    Signed-off-by: Dan Cross <cross@gajendra.net>

---
+++ syslib/src/stat.rs
@@ -0,0 +1,16 @@
+#[repr(u32)]
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum FileType {
+    Unused = 0,
+    Dir = 1,
+    File = 2,
+    Dev = 3,
+}
+
+pub struct Stat {
+    typ: FileType,
+    dev: u32,
+    ino: u64,
+    nlink: u32,
+    size: u64,
+}
